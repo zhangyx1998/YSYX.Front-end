6ecf72601771cca60bf481b7dc5914519de7d057
ssw1999 <805405608@qq.com>
2021-11-09 18:08:54 +0800

    [Apply] Prompt information correctly when submission fails

--- apply/UI/Common/popup.vue
+++ apply/UI/Common/popup.vue
@@ -8,17 +8,25 @@ defineProps({
 <template>
 	<div v-show="popShow" class="popup">
 		<div class="success">
-			<div>
+			<div v-if="submitData.valid">
 				<svg width="120" height="120" viewBox="0 0 40 40">
-					<circle cx="50%" cy="50%" r="19" class="circle"></circle>
+					<circle cx="50%" cy="50%" r="19" class="validCircle"></circle>
 					<polyline
 						points="5,20 15,30 35 10"
 						class="hookmark"
 					></polyline>
 				</svg>
 			</div>
-			<div class="submit_success">提交成功</div>
-			<div class="ID_number">ID:{{submitData.ID}}</div>
+			<div v-if="!submitData.valid">
+				<svg width="120" height="120" viewBox="0 0 40 40">
+					<circle cx="50%" cy="50%" r="19" class="invalidCircle"></circle>
+					<line x1="10" y1="10" x2="30" y2="30" class="hookmark"></line>
+					<line x1="30" y1="10" x2="10" y2="30" class="hookmark"></line>
+				</svg>
+			</div>
+			<div v-if="submitData.valid" class="submit_success">提交成功</div>
+			<div v-if="!submitData.valid" class="submit_success">提交失败</div>
+			<div class="ID_number">ID:{{ submitData.id }}</div>
 			<div class="link">
 				<a href="#" alt="home" style="padding-right: 6em">返回首页</a>
 				<a href="#" alt="detail" style="padding-left: 6em">查看详情</a>
@@ -77,14 +85,18 @@ export default {
 	stroke: var(--white);
 	stroke-width: 1px;
 }
-.circle {
-	/* animation: circleTransform 1s 0.6s linear both; */
+.validCircle {
 	stroke: var(--green-bright);
 	stroke-width: 2px;
 	fill: var(--green-bright);
 	stroke-dasharray: 320;
 }
-
+.invalidCircle {
+	stroke: var(--red-bright);
+	stroke-width: 2px;
+	fill: var(--red-bright);
+	stroke-dasharray: 320;
+}
 @keyframes hookmarkTransform {
 	from {
 		stroke-dasharray: 100 100;
@@ -95,12 +107,4 @@ export default {
 		stroke-dashoffset: 0;
 	}
 }
-/* @keyframes circleTransform {
-  from {
-	stroke-dashoffset: 320;
-  }
-  to {
-	stroke-dashoffset: 0;
-  }
-} */
 </style>
